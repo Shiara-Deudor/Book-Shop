66611d099b13abb7bcd8a94b99ad66659f933a42
shiara02 <shiara@bpoc.co.jp>
2023-11-30 17:05:22 +0800

    packages updates

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(process.env.PORT)
+  },
+  define: {
+    'process.env.PORT': `${process.env.PORT}`
+  },
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
