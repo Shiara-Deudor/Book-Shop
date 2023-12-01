0477b2ff4eb004665a715d68a9ff4ba761214820
shiara02 <shiara@bpoc.co.jp>
2023-12-01 11:55:40 +0800

    featured books section added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './home.scss';
+import Featured from '../../layouts/featured/Featured';
 
 const Home = () => {
   return (
-    <section className='landingPage'>
+    <section>
+      <div className='landingPage'>
         <h2>
             “No book is really worth reading at the age of ten <br></br> 
         which is not equally – and often far more – worth reading 
         <br></br> at the age of fifty and beyond.” <br></br> C.S. Lewis
         </h2>
+      </div>
+      <Featured />
     </section>
   )
 }
