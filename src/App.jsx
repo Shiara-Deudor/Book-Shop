6481652bfe56f0cd0e0321ec74f9c3eaec97b88e
shiara02 <shiara@bpoc.co.jp>
2023-12-05 15:33:24 +0800

    routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import { useState } from 'react'
-import '@/styles/index.scss'
-import Header from './layouts/header/header'
-import Home from './pages/home/home'
-import Footer from './layouts/footer/Footer'
+import React from 'react';
+import '@/styles/index.scss';
+import Header from './layouts/header/Header';
+import Home from './pages/home/Home';
+import Footer from './layouts/footer/Footer';
+import Shop from './pages/shop/Shop';
+import Cart from './pages/cart/Cart';
+import Likes from './pages/likes/Likes';
+import Profile from './pages/profile/Profile';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-
   return (
-    <>
-      <Header />
-      <Home />
-      <Footer />
-    </>
-  )
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/likes" element={<Likes />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
