97458aa61f644c2d89ecfdf1787540bca416ad5c
shiara02 <shiara@bpoc.co.jp>
2023-12-07 14:22:44 +0800

    initial book items list

---
+++ src/layouts/booklist/BookList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './booklist.scss';
+import BookCard from '../../components/bookcard/BookCard'
+
+
+const BookList = ({ booksData }) => {
+  return (
+    <div className='book_list'>
+      {booksData.map((book, index) => {
+        return (
+          <BookCard
+            key={index}
+            image={book.volumeInfo.imageLinks.thumbnail}
+            title={book.volumeInfo.title}
+            authors={book.volumeInfo.authors}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default BookList;
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import './shop.scss';
+import axios from 'axios';
+import BookList from '../../layouts/booklist/BookList';
 
 const Shop = () => {
-  return (
-    <div>Shop</div>
-  )
-}
+  const [booksData, setBooksData] = useState([]);
+  const [page, setPage] = useState([1]);
+  const [loading, setLoading] = useState(true);
 
-export default Shop
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://www.googleapis.com/books/v1/volumes?q=the&filter=free-ebooks&printType=books&key="
+      );
+        setBooksData((prev) => [...prev, ...response.data.items]);
+        setLoading(false)
+      }
+    fetchData();
+  }, [page]);
+
+  const handleScroll = () => {
+    if (
+        window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight
+    ) {
+      setLoading(true)
+      setPage(prev => prev+1);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  })
+
+  return (
+    <section className='books'>
+      <div className='books_items'>
+        <h1>SHOP NOW</h1>
+        <BookList booksData={booksData} className="books_data"/>
+      </div>
+    </section>   
+  );
+};
+
+export default Shop;
